55d5ca62a530876d820918d8d5a8222460fb72a8
solidsniper <bauer__alex@hotmail.com>
2021-11-27 19:19:22 +0100

    adds fonts and chezmoi alias

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo pacman -S bitwarden-cli --noconfirm
-
-bw config server https://bitwarden.stryzen.com
-
-export BW_SESSION=$(bw unlock --raw $1)
-
-[ ! -z "$BW_SESSION" ] && echo "Login successful" && chezmoi apply -v
