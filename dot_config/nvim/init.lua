e8cd8d1e01048a452e4958d4cb789da475b0f293
solidsniper <bauer__alex@hotmail.com>
2026-01-05 20:19:20 +0100

    automatically apply changes in chezmoi dir

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -219,6 +219,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- automatically run chezmoi apply in chezmoi directory
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { os.getenv 'HOME' .. '/.local/share/chezmoi/*' },
+  callback = function(ev)
+    local bufnr = ev.buf
+    local edit_watch = function()
+      require('chezmoi.commands.__edit').watch(bufnr)
+    end
+    vim.schedule(edit_watch)
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -289,6 +301,40 @@ require('lazy').setup({
       -- This option is required.
       vim.g['chezmoi#use_tmp_buffer'] = true
       -- add other options here if needed.
+    end,
+  },
+  {
+    'xvzc/chezmoi.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('chezmoi').setup {
+        events = {
+          on_open = {
+            notification = {
+              enable = true,
+              msg = 'Opened a chezmoi-managed file',
+              opts = {},
+            },
+          },
+          on_watch = {
+            notification = {
+              enable = true,
+              msg = 'This file will be automatically applied',
+              opts = {},
+            },
+          },
+          on_apply = {
+            notification = {
+              enable = true,
+              msg = 'Successfully applied',
+              opts = {},
+            },
+          },
+        },
+        telescope = {
+          select = { '<CR>' },
+        },
+      }
     end,
   },
 
