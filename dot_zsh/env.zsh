7ce096843f260c3b72d461c620fb73ad40d6f2cb
solidsniper <bauer__alex@hotmail.com>
2026-01-06 02:43:47 +0100

    nvm is not needed when using nodenv

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -1,4 +1,4 @@
 export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:~/.nodenv/shims
 export TERM=xterm-256color
 export MANPAGER="nvim +Man!"
