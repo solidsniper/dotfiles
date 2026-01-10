33a19dc06c945674a286a0db75dcabb1e176157d
solidsniper <bauer__alex@hotmail.com>
2026-01-10 14:53:33 +0100

    add mpv config file and fix colors in fullscreen

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -1,4 +1,5 @@
 export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:~/.nodenv/shims
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:
+export PATH=$PATH:~/.nodenv/shims
 export TERM=xterm-256color
 export MANPAGER="nvim +Man!"
