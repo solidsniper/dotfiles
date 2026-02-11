4ec4eaec37ecd86273b2a2708a68b98f17a46507
solidsniper <bauer__alex@hotmail.com>
2026-02-11 23:14:38 +0100

    fix error in which-key config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -396,8 +396,6 @@ require('lazy').setup({
         { '<leader>c', group = '[C]ode' },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
-      }
-      spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
