b8e58be0216c15b6573b5edc878796ec85ed1a1a
solidsniper <bauer__alex@hotmail.com>
2026-02-11 23:12:05 +0100

    automatically remove unused imports on save using goimports-reviser

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -986,11 +986,10 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        go = { 'goimports-reviser', 'gofmt', stop_after_first = true },
+      },
+      formatters = {
+        ['goimports-reviser'] = { prepend_args = { '-rm-unused' } },
       },
     },
   },
