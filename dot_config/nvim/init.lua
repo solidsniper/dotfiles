c0fbf3fad9a5e7479d2e2e8efe973bd7b10cad1b
solidsniper <bauer__alex@hotmail.com>
2026-02-27 23:19:56 +0100

    adds buffer to luasnip completion

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -939,7 +939,7 @@ require('lazy').setup({
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'buffer', 'snippets', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
